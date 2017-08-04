b2596c529bf04ecc16971c790a0d4c242a756523
TrevJonez <trevorjones141@gmail.com>
2017-08-04 07:38:33 -0600

    show and hide based on the tabs.

--- reportJs/src/main/kotlin/KontrastReportIndex.kt
+++ reportJs/src/main/kotlin/KontrastReportIndex.kt
@@ -1,3 +1,11 @@
+import org.w3c.dom.HTMLCollection
+import org.w3c.dom.asList
+import org.w3c.dom.get
+import kotlin.browser.document
+import kotlin.browser.window
+import kotlin.dom.addClass
+import kotlin.dom.removeClass
+
 /*
  *    Copyright 2017 Trevor Jones
  *
@@ -15,5 +23,41 @@
  */
 
 fun main(args: Array<String>) {
+    window.onload = {
+        document.getElementsByClassName("AllTab")[0]?.addEventListener("click", { event ->
+            document.getElementsByClassName("skipped").showAll()
+            document.getElementsByClassName("success").showAll()
+            document.getElementsByClassName("failed").showAll()
+        })
 
+        document.getElementsByClassName("PassedTab")[0]?.addEventListener("click", { event ->
+            document.getElementsByClassName("skipped").hideAll()
+            document.getElementsByClassName("success").showAll()
+            document.getElementsByClassName("failed").hideAll()
+        })
+
+        document.getElementsByClassName("FailedTab")[0]?.addEventListener("click", { event ->
+            document.getElementsByClassName("skipped").hideAll()
+            document.getElementsByClassName("success").hideAll()
+            document.getElementsByClassName("failed").showAll()
+        })
+
+        document.getElementsByClassName("SkippedTab")[0]?.addEventListener("click", { event ->
+            document.getElementsByClassName("skipped").showAll()
+            document.getElementsByClassName("success").hideAll()
+            document.getElementsByClassName("failed").hideAll()
+        })
+    }
+}
+
+private fun HTMLCollection.hideAll() {
+    asList().forEach {
+        it.addClass("hidden-test-case")
+    }
+}
+
+private fun HTMLCollection.showAll() {
+    asList().forEach {
+        it.removeClass("hidden-test-case")
+    }
 }
