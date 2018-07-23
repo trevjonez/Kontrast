0b86d572508106a8805b3ddfd71e8383f9ee3f26
TrevJonez <trevorjones141@gmail.com>
2018-07-22 18:21:07 -0600

    workstation_sync

--- runner/src/main/kotlin/com/trevjonez/kontrast/runner/KontrastTestRunner.kt
+++ runner/src/main/kotlin/com/trevjonez/kontrast/runner/KontrastTestRunner.kt
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2017 Trevor Jones
+ *    Copyright 2018 Trevor Jones
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -14,6 +14,7 @@
  *    limitations under the License.
  */
 
-rootProject.name = 'Kontrast'
+package com.trevjonez.kontrast.runner
 
-include ':plugin', ':androidTestClient', ':appClient', ':app', ':unitTestClient', ':reportJs', ':jvmCommon', ':runner'
+class KontrastTestRunner{
+}
